1f78d3d8900871a4762e157e05b4e6348d163119
Robert Engelbert <Robe32002@yahoo.com>
2016-06-29 08:10:46 -0600

    A multiple line if elseif statement from chapter 4 of the textbook for the deep dive coders bootcamp

---
+++ ifelseif.php
@@ -0,0 +1,8 @@
+<?php
+$page = "";
+if ($page == "Home") echo "You selected Home";
+elseif ($page == "About") echo "You selected About";
+elseif ($page == "News") echo "You selected News";
+elseif ($page == "Login") echo "You selected Login";
+elseif ($Page == "Links") echo "You selected Links";
+?>
