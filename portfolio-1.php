fe2337e11d30f851c9192b1aaf2e51c5b357e8b6
Robert Engelbert <Robe32002@yahoo.com>
2016-07-31 10:26:22 -0600

    Restarted bootstrap site

--- portfolio-1.php
+++ portfolio-1.php
@@ -1,18 +1,52 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
-		<meta charset="utf-8" />
-		<title>Portfolio</title>
+		<title>PWP++</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	</head>
 	<body>
-		<header>
-			<h1>Robert Engelbert</h1>
-			<nav>
-				<a>Home</a>
-				<a>Portfolio</a>
-				<a>About</a>
-				<a>Contact</a>
-			</nav>
-		</header>
+			<div class="container-fluid">
+
+				<!-- Logo -->
+				<nav class="navbar navbar-default">
+				<div  class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+					</button>
+					<a href="#" class="navbar-brand">Robert Engelbert</a>
+				</div>
+
+				<!-- Menu Items -->
+				<div class="collapse navbar-collapse" id="mainNavBar">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li class="active"><a href="#">Portfolio</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+
+			</div>
+		
+
+		<!--page content-->
+		<div class="container text-center">
+			<h2 class="display-2">Portfolio</h2>
+			<p class="text-center lead">Welcome to mysite please browse around and give me some money before you leave.</p>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 text-center">
+					Example project coming soon
+				</div>
+				<div class="col-md-6 text-center">
+					Example project coming soon
+				</div>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	</body>
 </html>
